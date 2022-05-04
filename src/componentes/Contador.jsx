dc8cb29bd89f04d5518504346e57155b1bd6d051
Alonso Bustamante <carlosaj.bustamante@gmail.com>
2022-05-04 02:25:22 -0500

    Carga inicial del proyecto

---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,11 @@
+import '../hojas-de-estilo/Contador.css'
+
+function Contador({ numClics }) {
+  return (
+    <div className='contador'>
+      {numClics}
+    </div>
+  );
+}
+
+export default Contador;
